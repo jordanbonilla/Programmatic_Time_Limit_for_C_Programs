af504e424763be3938f994db2a0e3f3290baf74f
Jordan Bonilla <jbonilla@caltech.edu>
2016-04-04 20:33:57 -0700

    Update enforce_time_limit.c

--- enforce_time_limit.c
+++ enforce_time_limit.c
@@ -29,45 +29,45 @@ void enforce_time_limit(int time_limit) {
     // If it hasn't already terminated.
     if(child_id == 0) // "I am the child thread"
     {
-	sleep(time_limit);
+    sleep(time_limit);
         if( kill(parent_id, SIGTERM) == 0) {
-        	printf("enforce_time_limit.c: Program terminated"
-        	 " for taking longer than %d seconds\n", time_limit);
+            printf("enforce_time_limit.c: Program terminated"
+             " for taking longer than %d seconds\n", time_limit);
         }
         // Ensure that parent was actually terminated
         sleep(2);
         if( kill(parent_id, SIGKILL) == 0) {
-        	printf("enforce_time_limit.c: Program terminated"
-        	 " for taking longer than %d seconds\n", time_limit);
+            printf("enforce_time_limit.c: Program terminated"
+             " for taking longer than %d seconds\n", time_limit);
         } 
         // Child thread has done its job. Terminate now.
         exit(0);
     }
     else // "I am the parent thread"
     {
-	    // Allow the parent thread to continue doing what it was doing
-		return;
-	}
+        // Allow the parent thread to continue doing what it was doing
+        return;
+    }
 }
 
 // Entry point for your own C/C++ program.
 int main(int argc, char ** argv) {
-	// Call "enforce_time_limit" at the very beginning of your program
-	int max_exeuction_time_in_seconds = 10;
-	enforce_time_limit(max_exeuction_time_in_seconds);
+    // Call "enforce_time_limit" at the very beginning of your program
+    int max_exeuction_time_in_seconds = 10;
+    enforce_time_limit(max_exeuction_time_in_seconds);
 
-	// Proceed with your program
-	if(argc != 2) {
-		printf("./test <time_to_sleep (s)>\n");
-		return -1;
-	}
-	int time_to_sleep = atoi(argv[1]);
+    // Proceed with your program
+    if(argc != 2) {
+        printf("./test <time_to_sleep (s)>\n");
+        return -1;
+    }
+    int time_to_sleep = atoi(argv[1]);
 
-	// Visually show elapsed time
-	for(int i = 0; i < time_to_sleep; ++i) {
-		sleep(1);
-		printf("Time elapsed (s): %d\n", i + 1);
-	}
-	return 0;
+    // Visually show elapsed time
+    for(int i = 0; i < time_to_sleep; ++i) {
+        sleep(1);
+        printf("Time elapsed (s): %d\n", i + 1);
+    }
+    return 0;
 
 }
